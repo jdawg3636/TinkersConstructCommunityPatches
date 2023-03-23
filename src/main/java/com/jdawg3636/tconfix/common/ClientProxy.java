88a8fed5f3d7aa66b772d786263dec474c4e7ba8
jdawg3636 <jdawg3636minecraft@gmail.com>
2023-03-22 19:23:19 -0500

    Strip Irrelevant Functionality
    Some of this will need re-implemented down the line - want to keep it lean and mean for now

--- src/main/java/com/jdawg3636/tconfix/common/ClientProxy.java
+++
@@ -1,8 +0,0 @@
-package com.jdawg3636.tconfix.common;
-
-public class ClientProxy extends CommonProxy {
-
-    // Override CommonProxy methods here, if you want a different behaviour on the client (e.g. registering renders).
-    // Don't forget to call the super methods aswell.
-
-}
--- src/main/java/com/jdawg3636/tconfix/mixins/RapierMixin.java
+++ src/main/java/com/jdawg3636/tconfix/mixins/RapierMixin.java
@@ -1,18 +1,56 @@
 package com.jdawg3636.tconfix.mixins;
 
 import com.jdawg3636.tconfix.common.TConFix;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import tconstruct.items.tools.Rapier;
+import tconstruct.library.tools.Weapon;
+
+import java.util.List;
 
 @Mixin(Rapier.class)
-public class RapierMixin {
+public class RapierMixin extends Weapon {
 
     @Inject(method = "pierceArmor", at = @At("HEAD"), cancellable = true, remap = false)
     private void tconfix$DisableRapierPiercing(CallbackInfoReturnable<Boolean> callback) {
         callback.setReturnValue(false);
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public void addInformation(ItemStack stack, EntityPlayer player, List list, boolean gameSettingsAdvancedItemTooltips) {
+        super.addInformation(stack, player, list, gameSettingsAdvancedItemTooltips);
+    }
+
+    @Shadow
+    public String getIconSuffix(int i) { return null; }
+
+    @Shadow
+    public String getEffectSuffix() {
+        return null;
+    }
+
+    @Shadow
+    public String getDefaultFolder() {
+        return null;
+    }
+
+    @Shadow
+    public Item getHeadItem() {
+        return null;
+    }
+
+    @Shadow
+    public Item getAccessoryItem() {
+        return null;
     }
 
 }
