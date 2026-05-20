ecad8a0168b19e2dc9f3bb88fc573ac46d646e05
jdawg3636 <jdawg3636minecraft@gmail.com>
2026-05-20 04:04:12 -0500

    Add Galacticraft to Environment

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,8 @@
 plugins {
     id("com.gtnewhorizons.gtnhconvention")
 }
+
+// Allows runClient to work despite some jank in Galacticraft's MicdoodleTransformer
+tasks.withType<JavaExec>().configureEach {
+    jvmArgs("-noverify")
+}
